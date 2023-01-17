0ff713197be9b19c0686e2c4e192241e1f1b68f7
Mauricio <vdwanderley@gmail.com>
2023-01-16 22:29:00 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+.Net Benchmarks as Proof of Concepts
---
+++ server/Function.cs
@@ -0,0 +1,21 @@
+using Google.Cloud.Functions.Framework;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace server;
+
+public class Function : IHttpFunction
+{
+    /// <summary>
+    /// Delay an HTTP Response
+    /// </summary>
+    /// <param name="context">The HTTP context, containing the request and the response.</param>
+    public async Task HandleAsync(HttpContext context)
+    {
+        var delay = Convert.ToInt32(context.Request.Query["delay_mls"]);
+        await Task.Delay(delay);
+        context.Response.StatusCode = StatusCodes.Status200OK;
+        await Task.CompletedTask;
+    }
+}
